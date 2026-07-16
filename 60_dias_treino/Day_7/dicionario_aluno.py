d10aebd88229773abd7841f92374197b9095490b
Jhader-DevSec <jhaderaugusto78@gmail.com>
2026-07-15 21:06:39 -0300

    add: adição dias 7 a 21

---
+++ 60_dias_treino/Day_7/dicionario_aluno.py
@@ -0,0 +1,11 @@
+aluno = {
+    "nome": "Jhader",
+    "idade": 18,
+    "Curso": "Python do Zero ao Hacking - Ackerdemy",
+    "matricula": 182376342376
+}
+
+print("Nome:", aluno["nome"])
+print("Idade:", aluno["idade"])
+print("Curso:", aluno["Curso"])
+print("Matricula:", aluno["matricula"])
---
+++ 60_dias_treino/Day_9/contador.py
@@ -0,0 +1,15 @@
+def contador_personalizado():
+    try:
+        limite = int(input("Digite o valor maximo do contador: "))
+        #funcao range cria uma lista de numero a partir do 0
+        #ate o valor definido pelo usuario
+        limite = limite + 1
+        for i in range(limite):
+            print(i)
+            if i == limite:
+                print("Contador atingiu o limite")
+                break
+    except ValueError:
+        print("Entrada invalida. Por Favor insira um numero inteiro.")
+
+contador_personalizado()
